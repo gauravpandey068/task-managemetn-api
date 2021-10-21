d9b1ab1207290f9ffc55fd299d793e1e400cfa5e
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-10-21 10:04:12 +0545

    add todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TaskModule } from './task/task.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule, TodoModule],
   controllers: [],
   providers: [],
 })
