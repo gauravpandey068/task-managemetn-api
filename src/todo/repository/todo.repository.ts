6bfce7c41bc0a7e309aa78c3b98679dbe378bca3
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-01 18:15:56 +0545

    save todo items to its task

--- src/todo/repository/todo.repository.ts
+++ src/todo/repository/todo.repository.ts
@@ -5,7 +5,7 @@ import { TodoStatus } from '../utils/todo-status.enum';
 
 @EntityRepository(Todo)
 export class TodoRepository extends Repository<Todo> {
-  async createTodo(createTodoDto: CreateTodoDto): Promise<any> {
+  async createTodo(taskId: number, createTodoDto: CreateTodoDto): Promise<any> {
     const { todoName, todoDescription } = createTodoDto;
 
     const todo = new Todo();
@@ -13,6 +13,7 @@ export class TodoRepository extends Repository<Todo> {
     todo.todoName = todoName;
     todo.todoDescription = todoDescription;
     todo.status = TodoStatus.TODO; //default value
+    todo.taskId = taskId;
 
     await this.manager.save(todo);
 
