a1d0b1c14cf5beea4e955a964a8411c6235a20d1
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-16 11:21:16 +0545

    user auth entity

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,20 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class Auth {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
