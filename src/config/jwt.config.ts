aec40426928751515d7e30b8661a528fdd5a850b
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-19 08:57:42 +0545

    jwt secreatkey

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,4 @@
+export const jwtConfig = {
+  secretKey:
+    'secrzawxsedrvftbgynhumjikocfvbhnjmk123456789098123985202Q4W3EDRTYFGVJHB97OYUKTFGCWRTDYGVJwasrxdtfcgvjhbestfygvtygh!@#$%^&*estygvhb_)(*&^%erdyftghbrdytfhktyhv896iughsrdytfw457fgiyffghfddfyutrtyfdxudrguyfttdfhvh',
+};
