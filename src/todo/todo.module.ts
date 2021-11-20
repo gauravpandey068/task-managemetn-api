6b0e2457e5490dde99c97d145b87a7d917fb7dff
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-20 12:41:13 +0545

    imports AuthModule

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -4,9 +4,10 @@ import { TodoController } from './todo.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoRepository } from './repository/todo.repository';
 import { TaskModule } from 'src/task/task.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoRepository]), TaskModule],
+  imports: [TypeOrmModule.forFeature([TodoRepository]), TaskModule, AuthModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
