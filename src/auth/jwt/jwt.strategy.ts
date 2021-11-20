359c45c96ef3330e42969ae1e6d4b98958f15041
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-20 08:52:53 +0545

    jwt strategy

---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { jwtConfig } from 'src/config/jwt.config';
+import { Auth } from '../entities/auth.entity';
+import { UserRepository } from '../repository/auth.repository';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConfig.secretKey,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<Auth> {
+    const { email } = payload;
+    const user = await this.userRepository.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
