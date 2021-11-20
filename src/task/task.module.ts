18f9f6d2b7383a1d1852009986b306778a5eb760
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-11-20 11:18:43 +0545

    import auth module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,9 +3,10 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './repository/task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   controllers: [TaskController],
   providers: [TaskService],
   exports: [TaskService],
